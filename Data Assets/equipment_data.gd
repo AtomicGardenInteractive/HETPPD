c8d1b5438b491ee2c3feeaa4ac0c47ae7d7e7fbb
Brooke Heathcock <AtomicGardenInteractive@Gmail.com>
2024-04-08 14:49:52 +0100

    Level Layouts

---
+++ Data Assets/equipment_data.gd
@@ -0,0 +1,7 @@
+extends Resource
+class_name Equipment_Data
+
+@export var name: String
+@export var mesh: Mesh
+@export var animations: AnimationLibrary 
+@export var dialogueFile: JSON
--- Scenes/Player/EquipmentState.gd
+++ Scenes/Player/EquipmentState.gd
@@ -3,12 +3,21 @@ class_name EquipmentState
 
 signal EquipmentTransitioned
 var parent: Player
+@export var equipment: MeshInstance3D
 
 func enter():
 	var parent = $"."
+	if equipment == null:
+		pass
+	else:
+		equipment.visible=true
 	pass
 
 func exit():
+	if equipment == null:
+		pass
+	else:
+		equipment.visible=false
 	pass
 
 func Update(_delta: float):
