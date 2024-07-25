95b1e8102e80a9c41fd1f670d686b7927a9ad4ef
Brooke Heathcock <AtomicGardenInteractive@Gmail.com>
2024-07-25 16:25:12 +0100

    Menu Updates

---
+++ Scenes/Interface/main_menu_ui.gd
@@ -0,0 +1,25 @@
+extends Control
+
+@export var mainmenuscene : Node3D
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_play_pressed():
+	$"../../AnimationPlayer".play("Start Game")
+	pass # Replace with function body.
+
+
+func _on_options_pressed():
+	pass # Replace with function body.
+
+
+func _on_quit_pressed():
+		get_tree().quit()
